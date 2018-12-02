4ce9055bddb2cb1b4d243d70980ab0a0610b49f9
MasonLiuChn <MasonLiuChn@gmail.com>
2018-12-02 23:06:07 +0800

    add device id

--- lib_weex/src/main/java/com/masonliu/lib_weex/util/CommonUtil.java
+++ lib_weex/src/main/java/com/masonliu/lib_weex/util/CommonUtil.java
@@ -1,9 +1,12 @@
 package com.masonliu.lib_weex.util;
 
+import android.app.Service;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 
@@ -119,12 +122,9 @@ public class CommonUtil {
             sysOptionMap.put("androidStatusBarHeight", height);
 
             //国家代码
-            String countryCode;
             TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-            String countryIso = tm.getSimCountryIso();//返回SIM卡提供商的国家代码
-            if (!TextUtils.isEmpty(countryIso)) {
-                countryCode = countryIso;
-            } else {
+            String countryCode = tm.getSimCountryIso();//返回SIM卡提供商的国家代码
+            if (TextUtils.isEmpty(countryCode)) {
                 countryCode = Locale.getDefault().getCountry(); //CN
             }
             sysOptionMap.put("countryCode", countryCode);
@@ -132,7 +132,56 @@ public class CommonUtil {
             //获取系统当前使用的语言
             Locale locale = Locale.getDefault();
             sysOptionMap.put("language", locale.getLanguage() + "-" + locale.getCountry());
+
+            //获取系统当前使用的语言
+            sysOptionMap.put("deviceId", getTrueDeviceId(context));
         }
         map.putAll(sysOptionMap);
+    }
+
+    /**
+     //     * 获取 设备系统唯一ID
+     //     * 1、刷机或升级以后会变
+     //     * 2、相同品牌型号手机可能相同
+     //     * 3、可以根据 deviceId 、Build.BRAND、Build.MODEL 粗略判断是否是模拟器
+     //     * 模拟器deviceId一般全0，Build.BRAND一般是generic，Build.MODEL一般是sdk或google_sdk
+     //     * 4、root后的手机 用户可以随意修改deviceid 和 androidid
+     //     * 5、此方法返回一个不可靠的设备系统唯一ID
+     //     *
+     //     * @return
+     //     */
+    public static String getTrueDeviceId(Context context) {
+        try{
+            /**
+             * 非手机设备： 如果只带有Wifi的设备或者音乐播放器没有通话的硬件功能的话就没有这个DEVICE_ID
+             * bug：在少数的一些手机设备上，该实现有漏洞，会返回垃圾，如:zeros或者asterisks的产品
+             * 这个还需要权限
+             */
+//            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
+//            String deviceId = telephonyManager.getDeviceId();
+//            if (TextUtils.isEmpty(deviceId)) {
+//                deviceId = "NoDeviceId";
+//            }
+            String serial = Build.SERIAL;
+            if (TextUtils.isEmpty(serial)) {
+                serial = "NoSerial";
+            }
+            /**
+             * ANDROID_ID是设备第一次启动时产生和存储的64bit的一个数，当设备被wipe后该数重置
+             * ANDROID_ID似乎是获取Device ID的一个好选择，但它也有缺陷：
+             * 它在Android <=2.1 or Android >=2.3的版本是可靠、稳定的，但在2.2的版本并不是100%可靠的
+             * 在主流厂商生产的设备上，有一个很经常的bug，就是每个设备都会产生相同的ANDROID_ID：9774d56d682e549c
+             */
+            String androidID = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+            if (TextUtils.isEmpty(androidID)) {
+                androidID = "NoAndroidId";
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append(serial).append("#").append(androidID);
+            return sb.toString();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
     }
 }
